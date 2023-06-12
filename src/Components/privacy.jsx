aebc76576579e4649760f7729284892f324ca073
Yasir Khan <Yasirkahn6499@gmail.com>
2023-06-12 07:47:07 +0100

    first commit

---
+++ src/Components/privacy.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+const Privacy = () => {
+    return ( 
+        <div className="privacy-section">
+        <h2>Privacy</h2>
+        <p className="privacy-content">
+            
+        </p>
+        </div>
+     );
+}
+ 
+export default Privacy;
