208c7b0005ee4558b8ec0f46b28765327f092d0e
Yasir Khan <Yasirkahn6499@gmail.com>
2023-06-16 03:58:01 +0100

    Final tweeking!

--- src/Components/privacy.jsx
+++ src/Components/privacy.jsx
@@ -5,8 +5,41 @@ const Privacy = () => {
         <div className="privacy-section">
         <h2>Privacy</h2>
         <p className="privacy-content">
-            
-        </p>
+        At Games Rating By Public, we are committed to protecting the privacy and data of our users. This privacy policy outlines how we collect, use, and safeguard any information provided by users on our website.
+
+<h4>Information We Collect:</h4>
+<ol>
+<li>Email Address: When users sign up, we collect their email address for account registration and communication purposes.</li>
+<li>Game Ratings: Users can rate games using our platform. These ratings are anonymous and not linked to personally identifiable information.</li>
+</ol>
+
+<h4>Data Usage:</h4>
+<ol>
+<li>Account Management: We use the provided email address for user account management and communication regarding their account and the services we offer.</li>
+<li>Game Ratings: User ratings contribute to the collective rating of games on our platform and are displayed to other users anonymously.</li>
+</ol>
+
+<h4>Data Security:</h4>
+<ol>
+<li>We prioritize the security of user data and have implemented appropriate measures to prevent unauthorized access, disclosure, or alteration.</li>
+<li>We do not share personal information with third parties or sell user data to any external entities.</li>
+</ol>
+<h4>Data Removal:</h4>
+<ol>
+<li>Users can request the removal of their data from our database by emailing us at yasirkhan6499@gmail.com.</li>
+<li>Upon receiving a request, we will promptly remove the user's data from our systems.</li> 
+</ol>
+<h4>Cookies:</h4>
+<ol>
+We may use cookies on our website to enhance user experience and gather anonymous analytics data. Users have the option to disable cookies in their web browsers.
+</ol>
+
+
+
+<h4>Updates to Privacy Policy:</h4>
+<ol>
+We reserve the right to update or modify this privacy policy as needed. Any changes will be communicated through our website or via email.
+ </ol>       </p>
         </div>
      );
 }
